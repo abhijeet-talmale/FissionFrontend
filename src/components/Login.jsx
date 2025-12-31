1fe23896d20d45c9f1884dd29af5e7fa964d3e53
abhijeet-talmale <84791446+abhijeet-talmale@users.noreply.github.com>
2025-12-31 17:23:47 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,90 +1,106 @@
-import React from 'react'
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import '../assets/css/Sign.css';
+import React from "react";
+import { Formik } from "formik";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
 
 const Login = () => {
   const navigate = useNavigate();
+
   return (
-    <div className='container-fluid form-box p-3 d-flex flex-column justify-content-center  shadow border-2'>
-        <p className='fs-4'>Sign in to  account</p>
-        
-        <Formik
-       initialValues={{ email: '', password: '' }}
-       validate={values => {
-         const errors = {};
-         if (!values.email) {
-           errors.email = 'Required';
-         } else if (
-           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-         ) {
-           errors.email = 'Invalid email address';
-         }
-         return errors;
-       }}
-       onSubmit={async (values, { setSubmitting }) => {
-  try {
-    const res = await axios.post("https://fisssionbackendpro.onrender.com/login", {
-      email: values.email,
-      password: values.password
-    });
+    <div className="container p-4 shadow rounded" style={{ maxWidth: 420 }}>
+      <h3 className="mb-3">Login</h3>
 
-    alert("Login Successful");
-    navigate("/data");
-  } catch (err) {
-    console.log("LOGIN ERROR", err);
-    if (err.response) alert(err.response.data);
-    else alert("Server not running");
-  }
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.email) errors.email = "Email required";
+          if (!values.password) errors.password = "Password required";
+          return errors;
+        }}
+        onSubmit={async (values, { setSubmitting }) => {
+          try {
+            const res = await axios.post(
+              "https://fisssionbackendpro.onrender.com/login",
+              values,
+              { withCredentials: true }   // allow cookies if backend sends JWT cookie
+            );
 
-  setSubmitting(false);
-}}
->
-       {({
-         values,
-         errors,
-         touched,
-         handleChange,
-         handleBlur,
-         handleSubmit,
-         isSubmitting,
-         /* and other goodies */
-       }) => (
-         <form  className=" mb-3" onSubmit={handleSubmit}>
-           
-           <input
-           className='form-control'
-             type="email"
-             placeholder='Enter Your Email address'
-             name="email"
-             onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.email}
-             
-           />
-           <label className='label1'>Email</label>
-           {errors.email && touched.email && errors.email}
-           <input
-           className='form-control'
-             type="password"
-             name="password"
-             placeholder='Enter Your Password'
-             onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.password}
-           />
-           <label className='label1'>Password</label>
-           {errors.password && touched.password && errors.password}
-           <button className=" form-control btn"  type="submit" disabled={isSubmitting}>
-             Submit
-           </button>
-           <button type='submit'  className='btn btn danger' onClick={()=>navigate('/create')}>Create User</button>
-         </form>
-       )}
-     </Formik>
+            alert("Login Successful");
+
+            // If backend returns token
+            if (res.data.token) {
+              localStorage.setItem("token", res.data.token);
+            }
+
+            navigate("/data");
+          } catch (err) {
+            if (err.response) {
+              alert(err.response.data.message || "Login failed");
+            } else {
+              alert("Server not reachable");
+            }
+          }
+
+          setSubmitting(false);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              className="form-control mb-2"
+              placeholder="Email Address"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            {errors.email && touched.email && (
+              <small className="text-danger">{errors.email}</small>
+            )}
+
+            <input
+              type="password"
+              className="form-control mb-2"
+              placeholder="Password"
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            {errors.password && touched.password && (
+              <small className="text-danger">{errors.password}</small>
+            )}
+
+            <button
+              type="submit"
+              className="btn btn-primary w-100 mt-2"
+              disabled={isSubmitting}
+            >
+              Login
+            </button>
+
+            <button
+              type="button"
+              className="btn btn-outline-secondary w-100 mt-2"
+              onClick={() => navigate("/create")}
+            >
+              Create Account
+            </button>
+          </form>
+        )}
+      </Formik>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
